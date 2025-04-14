1d1a8b5c5c6e2fd989951856ea7ffec1cb5cb883
Keyi Wang <364059176w@gmail.com>
2025-04-14 18:59:36 -0500

    Create task2_stocks.py

---
+++ Task_2_starter_kit/task2_stocks.py
@@ -0,0 +1,45 @@
+import yfinance as yf
+import pandas as pd
+from datetime import datetime, timedelta
+
+
+def _get_stock_data(
+    tickers: list, start_date: str, end_date: str, filename: str = None
+):
+    """Download OHLCV data for a list of stock tickers and save it to a CSV file."""
+    all_data = pd.DataFrame()
+
+    for ticker in tickers:
+        print(f"Fetching data for {ticker}...")
+        stock_data = yf.download(ticker, start=start_date, end=end_date)
+
+        stock_data["Ticker"] = ticker
+
+        all_data = pd.concat([all_data, stock_data])
+        all_data
+
+    if filename:
+        all_data.to_csv(filename)
+        print(f"Data saved to {filename}")
+    return all_data  # .reset_index()
+
+
+def get_stock_data(tickers: list, start_date: str, end_date: str, filename: str = None):
+    return _get_stock_data(tickers, start_date, end_date, filename)
+
+
+if __name__ == "__main__":
+    get_stock_data(
+        [
+            "AAPL",
+            "NVDA",
+            "GOOG",
+            "AMZN",
+            "MSFT",
+            "XOM",
+            "WMT",
+        ],
+        "2020-01-01",
+        "2023-12-16",
+        "stocks.csv",
+    )
